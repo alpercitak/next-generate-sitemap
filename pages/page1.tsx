ac25d6aa42fc8761423c9689c850d1c484334fdf
Alper Çıtak <citak.alper@gmail.com>
2023-02-25 20:33:51 +0300

    prettification

--- pages/page1.tsx
+++ pages/page1.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
 const Page1: NextPage = () => {
-  return (
-    <div>Page1</div>
-  )
-}
+  return <div>Page1</div>;
+};
 
 export default Page1;
--- pages/page2.tsx
+++ pages/page2.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
 const Page2: NextPage = () => {
-  return (
-    <div>Page2</div>
-  )
-}
+  return <div>Page2</div>;
+};
 
 export default Page2;
