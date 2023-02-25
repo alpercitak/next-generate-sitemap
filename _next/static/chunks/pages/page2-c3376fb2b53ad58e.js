d1db4c9c220c61d7111c6388c220ed1c476c0c53
alpercitak <citak.alper@gmail.com>
2023-02-25 18:45:05 +0000

    Deploying to gh-pages from @ alpercitak/next-generate-sitemap@ebf03343f065a4f7db1e53676fed61daa2f838ff 🚀

---
+++ _next/static/chunks/pages/page2-c3376fb2b53ad58e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[730],{3541:function(n,u,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/page2",function(){return e(1857)}])},1857:function(n,u,e){"use strict";e.r(u);var _=e(5893);let t=()=>(0,_.jsx)("div",{children:"Page2"});u.default=t}},function(n){n.O(0,[774,888,179],function(){return n(n.s=3541)}),_N_E=n.O()}]);
