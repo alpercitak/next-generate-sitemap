3ec3b16a6751c2c55e3432f5436e8cdfde2d65cb
alpercitak <citak.alper@gmail.com>
2026-01-09 23:17:52 +0000

    Deploying to gh-pages from @ alpercitak/next-generate-sitemap@d3a04c0f8ebf190182c6b834960409ffafeb5797 🚀

--- _next/static/chunks/pages/page1-7ec366923df19cc0.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[356],{2400:function(n,u,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/page1",function(){return e(4861)}])},4861:function(n,u,e){"use strict";e.r(u);var _=e(5893);let t=()=>(0,_.jsx)("div",{children:"Page1"});u.default=t}},function(n){n.O(0,[774,888,179],function(){return n(n.s=2400)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/pages/page2-ab8313844f9e995f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[730],{3396:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/page2",function(){return _(3058)}])},3058:function(n,u,_){"use strict";_.r(u);var e=_(1527);u.default=()=>(0,e.jsx)("div",{children:"Page2"})}},function(n){n.O(0,[774,888,179],function(){return n(n.s=3396)}),_N_E=n.O()}]);
